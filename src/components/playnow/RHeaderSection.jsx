dad55089377046e23204d343033f68104a3f3e0a
irfan önk <57913715+irfanonk@users.noreply.github.com>
2022-03-28 17:32:27 +0300

    add play now page

---
+++ src/components/playnow/RHeaderSection.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function PNHeaderSection() {
+  return (
+    <div className="inpage-header-box text-center justify-center flex items-center h-[67px] min-h-full bg-[#051B5C]  ">
+      {/* <div className="flex flex-1 justify-start items-center ml-12 ">
+        <div className="flex text-center items-center">
+          <div className="text-xl mr-1 ">All Bloodline</div>
+          <img
+            className="w-3 h-4 rotate-90 "
+            src={require("../../assets/images/arrow.png")}
+            alt=""
+          />
+        </div>
+      </div> */}
+      <div className="flex flex-2 justify-center items-center gap-4">
+        <div className="text-yellow text-2xl font-bold uppercase">
+          live racing
+        </div>
+        <div className=" text-2xl font-bold uppercase">finished racing</div>
+      </div>
+      {/* <div className="flex flex-1 justify-end mr-12 gap-3">
+        <div className="flex text-center items-center">
+          <div className="text-xl mr-1 ">Gender</div>
+          <img
+            className="w-3 h-4 rotate-90 "
+            src={require("../../assets/images/arrow.png")}
+            alt=""
+          />
+        </div>
+        <div className="flex text-center items-center">
+          <div className="text-xl mr-1 ">Breed Type</div>
+          <img
+            className="w-3 h-4 rotate-90 "
+            src={require("../../assets/images/arrow.png")}
+            alt=""
+          />
+        </div>
+        <div className="flex text-center items-center">
+          <div className="text-xl mr-1 ">Sort By</div>
+          <img
+            className="w-3 h-4 rotate-90 "
+            src={require("../../assets/images/arrow.png")}
+            alt=""
+          />
+        </div>
+      </div> */}
+    </div>
+  );
+}
--- src/pages/PlayNow.jsx
+++ src/pages/PlayNow.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import PaginationSection from "../components/common/PaginationSection";
+import PNHeaderSection from "../components/playnow/RHeaderSection";
 
 export default function PlayNow() {
-  return <div>PlayNow</div>;
+  return (
+    <div className="p-10 ">
+      <div className="mb-10">
+        <PNHeaderSection />
+      </div>
+      {/* <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6">
+        {dogs.map((item, index) => (
+          <RItemCard key={index} item={item} key={index} />
+        ))}
+      </div> */}
+      <div className="mt-20">
+        <PaginationSection />
+      </div>
+    </div>
+  );
 }
