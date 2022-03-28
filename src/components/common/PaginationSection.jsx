c9a0a1d0fa8e6c1bef2b1c687c7c0c3e1c5ea960
irfan önk <57913715+irfanonk@users.noreply.github.com>
2022-03-28 15:18:19 +0300

    pagination styling

--- src/components/common/PaginationSection.jsx
+++ src/components/common/PaginationSection.jsx
@@ -2,16 +2,47 @@ import React from "react";
 
 export default function PaginationSection() {
   return (
-    <div className="flex  items-center h-[67px] min-h-full bg-[#051B5C] ">
-      <div class="flex-1  ">01</div>
-      <div class="flex flex-2 justify-center items-center gap-x-4  ">
+    <div className="pagination-box text-center justify-center flex items-center h-[67px] min-h-full bg-[#051B5C]  ">
+      <div class="flex flex-1 justify-start items-center ml-8">
+        <img
+          className="w-3 h-4 rotate-180 mr-1 "
+          src={require("../../assets/images/arrow.png")}
+          alt=""
+        />
+        <div className="text-l mr-2">Back Page</div>
+        <div className="text-l mr-1">Next Page</div>
+        <img
+          className="w-3 h-4 "
+          src={require("../../assets/images/arrow.png")}
+          alt=""
+        />
+      </div>
+      <div class="flex flex-2 justify-center  items-center gap-x-4  ">
+        <img
+          className="w-6 h-8 rotate-180"
+          src={require("../../assets/images/arrow.png")}
+          alt=""
+        />
         {Array.from(Array(15).keys()).map((item) => (
           <div className="w-[29px] h-[29px] rounded-full border-2 border-[#2099AB] ">
             {item + 1}{" "}
           </div>
         ))}
+        <img
+          className="w-6 h-8"
+          src={require("../../assets/images/arrow.png")}
+          alt=""
+        />
       </div>
-      <div class="flex-1  ">03</div>
+      <div class="flex flex-1 items-center  justify-end mr-8  ">
+        <div className="text-l mr-1">Show On Page</div>
+        <div className="text-xl mr-1">50</div>
+        <img
+          className="w-3 h-4 rotate-90 "
+          src={require("../../assets/images/arrow.png")}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
