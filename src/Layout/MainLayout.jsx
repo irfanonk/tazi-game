dee24061ccf202bcb70b5e2aa651c8cc426baf9a
irfan önk <57913715+irfanonk@users.noreply.github.com>
2022-04-04 12:06:36 +0300

    add extra layer

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -4,39 +4,21 @@ import Navbar from "../components/navbar/Navbar";
 
 const Layout = ({ children }) => {
   const pathname = useLocation().pathname;
-  console.log("pathname,", pathname);
   const headerName = pathname.match("/breeding") ? "two" : "one";
-  console.log("headerName,", headerName);
   return (
     <>
       <main className="min-h-[100vh] text-white bg-bgblue bg-main-background bg-cover bg-no-repeat bg-center  ">
         <div className="container mx-auto">
-          <Navbar />
-          {/* shining blue elipse */}
-          {/* <div className="flex justify-center ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="843"
-              height="265"
-              viewBox="0 0 657 265"
-            >
-              <path
-                id="Path_4297"
-                data-name="Path 4297"
-                d="M328.5,0C509.926,0,657,17.685,657,39.5S509.926,79,328.5,79,0,61.315,0,39.5,147.074,0,328.5,0Z"
-                transform="translate(93 93)"
-                fill="#0110c8"
-                opacity="0.200"
-              />
-            </svg>
-          </div> */}
-
-          {/* 
-          <div className="h-16 bg-layout-header-one bg-auto bg-no-repeat bg-center bg-contain">
-
-          </div> */}
+          <div className="relative">
+            <Navbar />
+          </div>
 
           <div className="flex flex-col justify-center items-center ">
+            <img
+              className=" max-w-full h-auto -mt-[100px] -mb-[78px] mx-auto"
+              src={require("../assets/images/extra-bg-layer.png")}
+              alt=""
+            />
             <img
               className=" max-w-full h-auto  -mb-[22px] "
               src={require("../assets/images/roco.png")}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,46 +7,44 @@ export default function Navbar() {
   const location = useLocation();
   const path = location.pathname;
   return (
-    <div className="">
-      <div className="grid grid-cols-6 pt-5 ">
-        <div className=""></div>
-        <div className="col-start-2 col-span-4">
-          <div className="flex flex-row justify-center content-center gap-4">
-            {navItems.map((item, index) => (
-              <Link
-                key={index}
-                isActive={path === `/${item}`}
-                className={`${
-                  path === `/${item}` ? "text-yellow " : "text-white"
-                } text-md font-semibold uppercase hover:text-yellow transition duration-200`}
-                to={`/${item}`}
-              >
-                <div className="text-md uppercase text-blur-6 ">
-                  {item.replace("-", " ")}
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div className=" flex justify-end gap-2">
-          <div className="flex justify-end items-center ">
-            <img
-              className="inline object-cover w-12 h-12  rounded-md"
-              src={require("../../assets/images/avatar-image.png")}
-              alt="Profile image"
-            />
-          </div>
-          <div className="flex-1 ">
-            <div className="text-l font-semibold  ">Lorenzo</div>
-            <div className="flex flex-1 gap-x-2 items-center ">
-              <div className="w-[50%]  bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div
-                  className="bg-yellow h-2.5 rounded-full"
-                  style={{ width: "50%" }}
-                ></div>
+    <div className="grid grid-cols-6 pt-5 ">
+      <div className=""></div>
+      <div className="col-start-2 col-span-4">
+        <div className="flex flex-row justify-center content-center gap-4">
+          {navItems.map((item, index) => (
+            <Link
+              key={index}
+              isActive={path === `/${item}`}
+              className={`${
+                path === `/${item}` ? "text-yellow " : "text-white"
+              } text-md font-semibold uppercase hover:text-yellow transition duration-200`}
+              to={`/${item}`}
+            >
+              <div className="text-md uppercase text-blur-6 ">
+                {item.replace("-", " ")}
               </div>
-              <span className="text-xs font-semibold uppercase">Level 1</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className=" flex justify-end gap-2">
+        <div className="flex justify-end items-center ">
+          <img
+            className="inline object-cover w-12 h-12  rounded-md"
+            src={require("../../assets/images/avatar-image.png")}
+            alt="Profile image"
+          />
+        </div>
+        <div className="flex-1 ">
+          <div className="text-l font-semibold  ">Lorenzo</div>
+          <div className="flex flex-1 gap-x-2 items-center ">
+            <div className="w-[50%]  bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+              <div
+                className="bg-yellow h-2.5 rounded-full"
+                style={{ width: "50%" }}
+              ></div>
             </div>
+            <span className="text-xs font-semibold uppercase">Level 1</span>
           </div>
         </div>
       </div>
